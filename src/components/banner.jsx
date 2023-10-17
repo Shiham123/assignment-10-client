6efbd59ddc3cb7f14bbba936dd4edccbbfe36dc8
Shiham123 <s36651786@gmail.com>
2023-10-18 01:26:59 +0600

    banner done with responsive

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -4,9 +4,18 @@ const Banner = () => {
   return (
     <div className={styles.banner}>
       <div className={styles.textContainer}>
-        <h1 className="text-3xl text-white">
-          TechSavvy: Navigating the Digital Frontier
-        </h1>
+        <div className="flex justify-center items-center py-[15rem] px-8">
+          <h1 className="font-poppins text-white lg:text-5xl md:text-4xl text-xl tracking-widest text-center lg:leading-[5rem] md:leading-[3rem] leading-none">
+            <span className="uppercase text-black">
+              Grow day business with digital product
+            </span>
+            <br />
+            <br />
+            <span className="font-bold lg:text-6xl md:text-5xl uppercase text-2xl">
+              Chose your brand wisely
+            </span>
+          </h1>
+        </div>
       </div>
     </div>
   );
