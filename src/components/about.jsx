25fb677e9ea30ecbda4e4c16e1219067bcb8322e
Shiham123 <s36651786@gmail.com>
2023-10-18 10:56:35 +0600

    about section added and also responsive

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,102 @@
 const AboutPage = () => {
-  return <div>this is about page</div>;
+  return (
+    <div className="flex justify-center items-center flex-col flex-wrap mx-20 border-4 border-[#53ba00b3] hover:border-8 duration-500">
+      <h1 className="font-poppins text-4xl md:text-6xl lg:text-7xl font-bold text-black hover:text-[#53ba00b3] duration-500 hover:scale-110 my-20 hover:uppercase">
+        About us
+      </h1>
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="font-bold">&quot;Tech for Everyone&quot;</span> is a
+        broad concept that encompasses the idea of making technology accessible,
+        affordable, and beneficial to people from all walks of life. It is
+        centered around the belief that technology should not be a privilege
+        limited to a select few but should be inclusive and beneficial to
+        everyone in society. Here are some key details about the concept of
+        &quot;Tech for Everyone&quot;:
+      </p>
+      <br />
+
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl">Inclusivity:</span>
+        <span className="font-bold">&quot;Tech for Everyone&quot;</span>{' '}
+        promotes inclusivity, meaning that technology should be designed and
+        made available to individuals regardless of their age, gender,
+        socioeconomic status, physical abilities, or geographical location. This
+        ensures that technology serves as a tool for empowering a diverse range
+        of people.
+      </p>
+      <br />
+
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl">Affordability:</span>{' '}
+        To make technology accessible to everyone, it&apos;s important that
+        devices, software, and digital services are affordable. This could be
+        achieved through subsidies, government initiatives, or innovations that
+        reduce the cost of technology.
+      </p>
+      <br />
+
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl pr-8">
+          Digital Literacy:
+        </span>
+        Ensuring that everyone has the skills and knowledge to use technology is
+        a vital part of this concept. This involves providing education and
+        training in digital literacy to bridge the digital divide and empower
+        individuals to make the most of technology.
+      </p>
+      <br />
+
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl pr-8">
+          Access to the Internet:
+        </span>
+        In the modern world, access to the internet is almost synonymous with
+        access to technology.
+        <span className="font-bold">&quot;Tech for Everyone&quot;</span>{' '}
+        emphasizes the importance of providing affordable and reliable internet
+        access, even in remote or underserved areas.
+      </p>
+
+      <br />
+
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl pr-8">
+          Tailored Solutions:
+        </span>
+        Not everyone&apos;s technology needs are the same.
+        <span className="font-bold">&quot;Tech for Everyone&quot;</span>{' '}
+        encourages the development of tailored solutions that address specific
+        challenges or opportunities faced by different demographics. For
+        example, tech solutions for the elderly may focus on health monitoring
+        and communication, while those for children might revolve around
+        educational content.
+      </p>
+
+      <br />
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl pr-8">
+          Ethical Considerations:
+        </span>{' '}
+        Inclusivity also extends to ethical considerations.{' '}
+        <span className="font-bold">&quot;Tech for Everyone&quot;</span>{' '}
+        encourages the responsible development and use of technology. It
+        includes aspects such as data privacy, security, and ensuring that
+        technology does not exacerbate existing inequalities.
+      </p>
+
+      <br />
+      <p className="font-poppins leading-10 tracking-wide text-xl md:text-2xl lg:text-3xl hover:bg-[#53ba00b3] duration-500 p-4">
+        <span className="text-3xl md:text-4xl lg:text-5xl pr-8">
+          &quot;Tech for Everyone&quot;
+        </span>{' '}
+        is an inclusive and forward-looking concept that aims to harness the
+        full potential of technology for the betterment of society while leaving
+        no one behind. It recognizes that technology has the power to improve
+        people&apos;s lives, and that this potential should be realized by
+        everyone, regardless of their background or circumstances.
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
