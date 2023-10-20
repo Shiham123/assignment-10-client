56754d725389f92fe3d52a199aa0f1e86d43d4bd
Shiham123 <s36651786@gmail.com>
2023-10-20 18:59:33 +0600

    firebase env setup

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA4x8T1Q5_eou2CYEc92ZoUDwmD7ptsHvY',
-  authDomain: 'tech-project-4435d.firebaseapp.com',
-  projectId: 'tech-project-4435d',
-  storageBucket: 'tech-project-4435d.appspot.com',
-  messagingSenderId: '553037094384',
-  appId: '1:553037094384:web:1ba9507c38834e4f50d135',
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
